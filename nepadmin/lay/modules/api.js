e635eff9c65bcea538b097153f6c22da55497eb3
黄奔 <2740916018@qq.com>
2022-05-07 16:35:45 +0800

    增加计划目录下专家内容

--- nepadmin/lay/modules/api.js
+++ nepadmin/lay/modules/api.js
@@ -1,9 +1,10 @@
 //请求URL
-layui.define([],function(exports){
-    exports('api',{
-        login:'json/login.json',
-        getMenu:'json/menu.json',
-        getGoods:'json/goods.json',
-        getExpertReview:'json/expert-review-list.json'
-    });
-})
+layui.define([], function (exports) {
+  exports("api", {
+    login: "json/login.json",
+    getMenu: "json/menu.json",
+    getGoods: "json/goods.json",
+    getExpertReview: "json/expert-review-list.json",
+    expertTrial: "json/expert-trial.json",
+  });
+});
